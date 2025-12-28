b09eed43a899b42c9c203e2af0789b7d0b2d35c1
LeandroDeJesus-S <jstleandro@gmail.com>
2025-12-28 16:01:39 -0300

    fix: toggle_tab_bar.py kitten

--- toggle_tab_bar.py
+++ toggle_tab_bar.py
@@ -1,7 +1,7 @@
-from kitty.boss import Boss
+# type: ignore
+from kitty.boss import Boss  # noqa: I001
 from kittens.tui.handler import result_handler
 from kittens.tui.loop import debug
-from kitty.fast_data_types import get_options, set_options
 
 
 def main(args: list[str]) -> str:
@@ -10,27 +10,25 @@ def main(args: list[str]) -> str:
 
 @result_handler(no_ui=True)
 def handle_result(args: list[str], answer: str, target_window_id: int, boss: Boss) -> None:
-    """Toggle tab bar visibility. It accepts a single argument that is the name of the tab bar
-    style to use when visible. Defaults to "powerline"."""
+    """Toggle tab bar visibility. It accepts two arguments that is the tab bar
+    style to use when visible and if using powerline, the tab_powerline_style. Defaults to
+    powerline and angled.
+    """
     tab = getattr(boss, "active_tab", None)
     tm = getattr(boss, "active_tab_manager", None)
+    win = boss.window_id_map.get(target_window_id)
 
     if tm is None or tab is None:
         debug("No active tab or tab manager found")
         return
 
-    debug(f"Active tab manager: {tm}")
     is_hidden = tm.tab_bar_hidden
+    bar_style = args[1] if len(args) >= 2 else "powerline"  # noqa: PLR2004
 
-    bar_style = args[0] if args else "powerline"
-    opts = get_options()
-    opts.tab_bar_style = bar_style if is_hidden else "hidden"
-    set_options(opts)
+    tab_bar_style = bar_style if is_hidden else "hidden"
+    powerline_style = args[2] if len(args) >= 3 else "angled"  # noqa: PLR2004
 
-    debug(f"Setting tab bar style to {opts.tab_bar_style}")
-    debug(f"Toggled tab bar visibility for tab {tm.tab_bar_hidden}")
-
-    boss.apply_new_options(opts)
-    tab.apply_options(True)
-    tab.mark_tab_bar_dirty()
-    tm.tab_bar.apply_options()
+    cmd = ["load-config"]
+    cmd += ["-o", f"tab_bar_style={tab_bar_style}"]
+    bar_style == "powerline" and cmd.extend(["-o", f"tab_powerline_style={powerline_style}"])
+    boss.call_remote_control(win, cmd)
